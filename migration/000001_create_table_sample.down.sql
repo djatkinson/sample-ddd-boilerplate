c8787f83b7eab063cc3d2294ad2202fd50883d70
fitraaditama <m.fitra.a@smma.id>
2023-10-17 14:55:50 +0700

    fix table name

--- migration/000001_create_table_sample.down.sql
+++ migration/000001_create_table_sample.down.sql
@@ -1,1 +1,1 @@
-DROP TABLE IF EXISTS sample;
+DROP TABLE IF EXISTS samples;
--- migration/000001_create_table_sample.up.sql
+++ migration/000001_create_table_sample.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS sample (
+CREATE TABLE IF NOT EXISTS samples (
                                       id bigserial NOT NULL,
                                       name text,
                                       created_at timestamp,
